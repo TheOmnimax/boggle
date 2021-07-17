6c5109e9400f5f4d14bf2fc3b20bc9183b0eafc8
Max S. Haberman <max@maxshaberman.com>
2021-07-17 03:45:07 -0400

    Added finder for all words

--- source/boggle/find_words.js
+++ source/boggle/find_words.js
@@ -9,13 +9,17 @@ function wordFinder(lTable, wordDict, wordList) {
     for (let c = 0; c < numCols; c++) {
       let startLetter = row[c]
       console.log('Start letter:', startLetter)
-      checkNextLetter(r, c, startLetter, [[r, c]])
+      checkNextLetter(r, c, startLetter, [])
     }
   }
   console.log(allWords)
   return allWords
 
-  function checkNextLetter(r, c, builtWord) {
+  function checkNextLetter(r, c, builtWord, usedCoord) {
+    if(usedCoord.nestedIncludes([r, c])) {
+      return
+    }
+    usedCoord.push([r, c])
     builtWord = builtWord.toLowerCase()
     // console.log('Built so far:', builtWord)
     if (wordDict[builtWord]) {
@@ -27,48 +31,58 @@ function wordFinder(lTable, wordDict, wordList) {
 
     let nextLetter
 
+    let newR
+    let newC
+
     // Top row
     if (r > 0) {
-      if ((c > 0)) {
-        nextLetter = lTable[r - 1][c - 1]
+      newR = r - 1
+      newC = c - 1
+      if (c > 0) {
+        nextLetter = lTable[newR][newC]
         // console.log('Next letter:', nextLetter)
-        checkNextLetter(r - 1, c - 1, builtWord + nextLetter)
+        checkNextLetter(newR, newC, builtWord + nextLetter,usedCoord)
       }
 
-      nextLetter = lTable[r - 1][c]
-      checkNextLetter(r - 1, c, builtWord + nextLetter)
+      nextLetter = lTable[newR][c]
+      checkNextLetter(newR, c, builtWord + nextLetter, usedCoord)
 
-      if (c < numCols - 1) {
-        nextLetter = lTable[r - 1][c + 1]
-        checkNextLetter(r - 1, c + 1, builtWord + nextLetter)
+      newC = c + 1
+      if (newC < numCols) {
+        nextLetter = lTable[newR][newC]
+        checkNextLetter(newR, newC, builtWord + nextLetter, usedCoord)
       }
     }
 
     // Middle row
     if (c > 0) {
-      nextLetter = lTable[r][c - 1]
-      checkNextLetter(r, c - 1, builtWord + nextLetter)
+      newC = c - 1
+      nextLetter = lTable[r][newC]
+      checkNextLetter(r, newC, builtWord + nextLetter, usedCoord)
     }
-    if (c < numCols - 1) {
-      nextLetter = lTable[r][c + 1]
-      checkNextLetter(r, c, builtWord + nextLetter)
+    newC = c + 1
+    if (newC < numCols) {
+      nextLetter = lTable[r][newC]
+      checkNextLetter(r, newC, builtWord + nextLetter, usedCoord)
     }
 
     // Bottom row
-    if (r < numRows - 1) {
+    newR = r + 1
+    if (newR < numRows) {
       if (c > 0) {
-        nextLetter = lTable[r + 1][c - 1]
-        checkNextLetter(r + 1, c - 1, builtWord + nextLetter)
+        newC = c - 1
+        nextLetter = lTable[newR][newC]
+        checkNextLetter(newR, newC, builtWord + nextLetter, usedCoord)
       }
 
-      nextLetter = lTable[r + 1][c]
-      checkNextLetter(r + 1, c, builtWord + nextLetter)
+      nextLetter = lTable[newR][c]
+      checkNextLetter(newR, c, builtWord + nextLetter, usedCoord)
 
-      if (c < numCols - 1) {
-        nextLetter = lTable[r + 1][c + 1]
-        checkNextLetter(r + 1, c + 1, builtWord + nextLetter)
+      newC = c + 1
+      if (newC < numCols) {
+        nextLetter = lTable[newR][newC]
+        checkNextLetter(newR, newC, builtWord + nextLetter, usedCoord)
       }
     }
-
   }
 }
